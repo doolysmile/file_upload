085f6a08a1223fa68537adbec60a5825282d4491
KCH <doolysmile1@naver.com>
2022-09-07 00:51:18 +0900

    feat : 회원가입 후 바로 로그인, 파일 업로드

--- src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.ll.exam.app10.app.base;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +20,15 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/**").permitAll();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
